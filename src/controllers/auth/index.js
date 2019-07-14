061a591a497c392450b66667b5d75097741851ae
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-14 04:23:52 +0300

    Add login and logout methods to AuthController

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,73 +1,60 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const ObjectId = mongoose.Types.ObjectId;
+const axios = require('axios');
+// const dotenv = require('dotenv');
+const JWT = require('jsonwebtoken');
 
-const searchOrGetAll = ({ Task }) => async (req, res, next) => {
-	const { q = '' } = req.query;
+// dotenv.config();
+
+const isProd = process.env.NODE_ENV === 'production';
+const apiUrl = isProd ? '/api' : 'http://localhost:9000/api';
+
+const login = () => async (req, res, next) => {
+	const { username, password, isGuest } = req.body;
+	const credentials = {
+		username: '',
+		password: '',
+	};
+
+	if (isGuest) {
+		credentials.username = 'guest';
+		credentials.password = 'guestPassword';
+	} else {
+		credentials.username = username;
+		credentials.password = password;
+	}
 
 	try {
-		const tasks = await Task.find().search(q);
-		res.status(200).send({ tasks });
+		const response = await axios.post(`${apiUrl}/validate/user`, credentials);
+		const { userId } = response.data();
+
+		JWT.sign(
+			{ userId },
+			{ secret: process.env.JWT_SECRET },
+			{ expiresIn: process.env.SESSION_EXPIRES_IN },
+			(err, token) => {
+				if (err) return next(err);
+
+				req.session.cookie.accessToken = token;
+				res.status(200).send({ token });
+			}
+		);
 	} catch (e) {
 		next(e);
 	}
 };
 
-const getById = ({ Task }) => async (req, res, next) => {
-	const { _id } = req.params;
-
+const logout = () => async (req, res, next) => {
 	try {
-		const task = await Task.findById(_id);
-		res.status(200).send({ task });
-	} catch (e) {
-		next(e);
-	}
-};
+		req.session.destroy(err => {
+			if (err) return next(err);
 
-const create = ({ Task }) => async (req, res, next) => {
-	try {
-		const { description } = req.body;
-		const task = await Task.create({
-			_id: ObjectId(),
-			description,
+			res.status(200).send({ message: 'user was logged out successfuly' });
 		});
-		res.status(200).send({ task });
 	} catch (e) {
 		next(e);
 	}
 };
 
-const updateById = ({ Task }) => async (req, res, next) => {
-	const { _id } = req.params;
-
-	try {
-		const { description } = req.body;
-		const task = await Task.findByIdAndUpdate(_id, { description });
-		res.status(200).send({ task });
-	} catch (e) {
-		next(e);
-	}
-};
-
-const deleteById = ({ Task }) => async (req, res, next) => {
-	const { _id } = req.params;
-
-	try {
-		const task = await Task.findByIdAndDelete(_id);
-		res.status(200).send({ task });
-	} catch (e) {
-		next(e);
-	}
-};
-
-module.exports = models => {
-	const router = express();
-
-	router.post('/', create(models));
-	router.get('/', searchOrGetAll(models));
-	router.get('/:_id', getById(models));
-	router.patch('/:_id', updateById(models));
-	router.delete('/:_id', deleteById(models));
-
-	return router;
+module.exports = {
+	login,
+	logout,
 };
