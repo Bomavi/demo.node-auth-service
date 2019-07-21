1636baec95578f297a80ccd6e2b8466c2415831f
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-22 01:40:23 +0300

    Cleanup app entry point

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+/* npm imports: common */
 const express = require('express');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+// const cors = require('cors');
 
 /* Declare global rootRequire for "absolute" paths */
 global.rootRequire = name => require(`${__dirname}/${name}`);
 
+/* root imports: common */
 const { debugLogger } = rootRequire('utils');
 const { api } = rootRequire('routes');
 const {
@@ -18,9 +22,17 @@ const PORT = isProd ? process.env.PORT : process.env.DEV_PORT;
 const maxAge = process.env.SESSION_EXPIRES_IN * 1000 || 3600 * 1000;
 const app = express();
 
-/* Initialize express-session with REDIS */
-if (isProd) app.set('trust proxy', 1);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+/* Use CORS in non production env */
+// if (!isProd) {
+// 	app.options('*', cors({ credentials: true }));
+// 	app.use(cors({ credentials: true }));
+// }
+
+/* Initialize express-session with REDIS */
+app.set('trust proxy', 1);
 app.use(
 	session({
 		store: new redisStore(redisOptions),
@@ -36,7 +48,7 @@ app.use(
 );
 
 /* Initialize app routes */
-app.use('/auth', api());
+app.use('/services/auth', api());
 
 app.listen(PORT, () => {
 	debugLogger('app', `Server is running on port ${PORT}`);
